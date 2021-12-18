973c62f1b85e07189cb8cea9580d6bf0b78bb6aa
league <league@imac-14.attlocal.net>
2021-12-17 16:59:20 -0800

    working on world clocks

--- src/_07_California_Weather/CaliforniaWeather.java
+++ src/_07_California_Weather/CaliforniaWeather.java
@@ -2,10 +2,10 @@ package _07_California_Weather;
 
 import java.util.HashMap;
 
-import javax.swing.JFrame;
+
+
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+
 
 /*
  * OBJECTIVE:
@@ -38,28 +38,58 @@ public class CaliforniaWeather {
         HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
         
         // All city keys have the first letter capitalized of each word
+        
+        String[] options = {"city","weather condition", "temperature"};
+        
+        int input = JOptionPane.showOptionDialog(null, "search for the weather conditions of a given city in California", "weather in California",
+        		JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null);
 
-        String input = JOptionPane.showInputDialog("input a city in California");
-        String cityName = Utilities.capitalizeWords(input);
-        WeatherData datum = weatherData.get(cityName);
         
-        if( datum == null ) {
-            System.out.println("Unable to find weather data for: " + cityName);
-        } else {
-        	JOptionPane.showMessageDialog(null, cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
-            System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+        
+        
+        if(input == 0) {
+        	String input1 = JOptionPane.showInputDialog("input a city in California");
+            String cityName = Utilities.capitalizeWords(input1);
+            WeatherData datum = weatherData.get(cityName);
+            
+            if( datum == null ) {
+                System.out.println("Unable to find weather data for: " + cityName);
+            } else {
+            	JOptionPane.showMessageDialog(null, cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+                System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+            }
+        	
         }
         
-        String input2 = JOptionPane.showInputDialog("specify a weather condition");
-        
-        for(String city: weatherData.keySet()) {
-        	String cityWeatherSummary = weatherData.get(city).weatherSummary;
-        	if(cityWeatherSummary.contains(Utilities.capitalizeWords(input2))){
-        		System.out.println(city);
-        	}
+        if(input == 1) {
+        	String input2 = JOptionPane.showInputDialog("specify a weather condition");
+            String citiesWeather = "";
+            
+            for(String city: weatherData.keySet()) {
+            	String cityWeatherSummary = weatherData.get(city).weatherSummary;
+            	if(cityWeatherSummary.contains(Utilities.capitalizeWords(input2))){
+            		System.out.println(city);
+            		citiesWeather = citiesWeather + city+"\n";
+            	}
+            }
+            JOptionPane.showMessageDialog(null, citiesWeather);
         }
         
-        String input3 = JOptionPane.showInputDialog("enter a minimum and maximum temperature");
+        if(input == 2) {
+        	String input3 = JOptionPane.showInputDialog("enter a minimum temperature");
+            double tempMin = Double.parseDouble(input3);
+            String input4 = JOptionPane.showInputDialog("enter a maxiumum temperature");
+            double tempMax = Double.parseDouble(input4);
+            String citiesTemperature = "";
+            
+            for(String city: weatherData.keySet()) {
+            	Double cityTemperatureF = weatherData.get(city).temperatureF;
+            	if(cityTemperatureF > tempMin && cityTemperatureF < tempMax){
+            		citiesTemperature = citiesTemperature + city+"\n";
+            	}
+            }
+            JOptionPane.showMessageDialog(null, citiesTemperature);
+        }
 
 
     }
--- src/_08_World_Clocks/WorldClocks.java
+++ src/_08_World_Clocks/WorldClocks.java
@@ -1,12 +1,16 @@
 package _08_World_Clocks;
 
 import java.awt.event.ActionEvent;
+
+
 import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.Timer;
@@ -37,40 +41,70 @@ import javax.swing.Timer;
 public class WorldClocks implements ActionListener {
     ClockUtilities clockUtil;
     Timer timer;
-    TimeZone timeZone;
+    TimeZone timeZone0;
+    TimeZone timeZone1;
 
     JFrame frame;
     JPanel panel;
     JTextArea textArea;
+    JTextArea textArea1;
+    JButton button;
     
-    String city;
+    String city0;
+    String city1;
     String dateStr;
+    String dateStr1;
     String timeStr;
+    String timeStr1;
+    String userCity;
     
     public WorldClocks() {
         clockUtil = new ClockUtilities();
 
         // The format for the city must be: city, country (all caps)
-        city = "Chicago, US";
-        timeZone = clockUtil.getTimeZoneFromCityName(city);
+        city0 = "Chicago, US";
+        timeZone0 = clockUtil.getTimeZoneFromCityName(city0);
         
-        Calendar calendar = Calendar.getInstance(timeZone);
+        city1 = "San Diego, US";
+        timeZone1 = clockUtil.getTimeZoneFromCityName(city1);
+        
+        
+        Calendar calendar = Calendar.getInstance(timeZone0);
+        Calendar calendar1 = Calendar.getInstance(timeZone1);
+        
         String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
         String dayOfWeek = calendar.getDisplayName( Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
         dateStr = dayOfWeek + " " + month + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR);
         
+        String month1 = calendar1.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        String dayOfWeek1 = calendar1.getDisplayName( Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+        dateStr1 = dayOfWeek1 + " " + month1 + " " + calendar1.get(Calendar.DAY_OF_MONTH) + " " + calendar1.get(Calendar.YEAR);
+        
         System.out.println(dateStr);
+        System.out.println(dateStr1);
+        
+       
 
         // Sample starter program
+
         frame = new JFrame();
         panel = new JPanel();
         textArea = new JTextArea();
+        textArea1 = new JTextArea();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setSize(100, 100);
         frame.add(panel);
         panel.add(textArea);
-        textArea.setText(city + "\n" + dateStr);
+        panel.add(textArea1);
+        textArea.setText(city0 + "\n" + dateStr);
+        textArea.setText(city1 + "\n" + dateStr1);
+        button = new JButton();
+        panel.add(button);
+        button.addActionListener(this);
+        button.setText("add city");
+        
+        
         
         // This Timer object is set to call the actionPerformed() method every
         // 1000 milliseconds
@@ -80,13 +114,27 @@ public class WorldClocks implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
-        Calendar c = Calendar.getInstance(timeZone);
+        Calendar c = Calendar.getInstance(timeZone0);
         String militaryTime = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
         String twelveHourTime = " [" + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND) + "]";
         timeStr = militaryTime + twelveHourTime;
         
+        Calendar c1 = Calendar.getInstance(timeZone1);
+        String militaryTime1 = c1.get(Calendar.HOUR_OF_DAY) + ":" + c1.get(Calendar.MINUTE) + ":" + c1.get(Calendar.SECOND);
+        String twelveHourTime1 = " [" + c1.get(Calendar.HOUR) + ":" + c1.get(Calendar.MINUTE) + ":" + c1.get(Calendar.SECOND) + "]";
+        timeStr1 = militaryTime1 + twelveHourTime1;
+        
         System.out.println(timeStr);
-        textArea.setText(city + "\n" + dateStr + "\n" + timeStr);
+        textArea.setText(city0 + "\n" + dateStr + "\n" + timeStr);
+        
+        System.out.println(timeStr1);
+        textArea1.setText(city1 + "\n" + dateStr1 + "\n" + timeStr1);
+        
+        if(arg0.equals(button)) {
+        	userCity = JOptionPane.showInputDialog("city name");
+        }
+        
+       
         frame.pack();
     }
 }
